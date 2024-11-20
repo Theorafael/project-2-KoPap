31dc280e6a700b4691873901036ae2b5554a82f7
Theorafael <Theorafaeltitahena@gmail.com>
2024-11-20 22:01:05 +0700

    modified and added some new codes. Nov, 20th 2024

--- assets/scripts/mainScript.js
+++
@@ -1,7 +0,0 @@
-let news = document.getElementById("myNewsBtn");
-news.addEventListener("click", function() {
-    document.getElementById("mySide").style.color = "red";
-    document.getElementById("mySide").style.width = "100%";
-    document.getElementById("myMain").style.display = "none";
-    console.log(news);
-})
