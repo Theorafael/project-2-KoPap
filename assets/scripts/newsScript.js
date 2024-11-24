1cbbcfd72dfdfebfa031d226a559d118df4cb72e
Theorafael <Theorafaeltitahena@gmail.com>
2024-11-24 23:48:28 +0700

    added new files & modified some files. Nov, 24th 2024

---
+++ assets/scripts/newsScript.js
@@ -0,0 +1,41 @@
+
+// Navbar
+function openNav() {
+    document.getElementById("myTopnav").style.width = "250px";
+    document.getElementById("newsMain").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+function closeNav() {
+    document.getElementById("myTopnav").style.width = "0";
+    document.getElementById("newsMain").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function myFunction() {
+    let nav = document.getElementById("myTopnav");
+    if (nav.className === "navbar") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "navbar";
+    }
+}
+
+let countDownDate = new Date("Jan 5, 2025 00:00:00").getTime();
+
+let x = setInterval(() => {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("myCountdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if(distance < 0) {
+        clearInterval(x);
+        document.getElementById("myCountdown").innerHTML = "EXECUTION";
+    }
+}, 1000);
