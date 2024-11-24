815c240319ec55c80565f7c878c6fc6fa3d0a78f
Theorafael <Theorafaeltitahena@gmail.com>
2024-11-24 14:42:52 +0700

    added new image & modified some files. Nov, 24 2024

--- assets/scripts/membersScript.js
+++ assets/scripts/membersScript.js
@@ -1,12 +1,25 @@
-// members.html
+// off-canvas side nav with opacity
+const cards = document.querySelectorAll(".card");
+cards.forEach(card => {
+  card.dataset.originalColor = "white";
+});
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("membersMain").style.marginLeft = "250px";
+  let allCards = document.querySelectorAll(".card");
+  allCards.forEach(card => {
+    card.style.backgroundColor = "rgba(0,0,0,0.7)";
+  });
+  document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("membersMain").style.marginLeft= "0";
+  let allCards = document.querySelectorAll(".card");
+  allCards.forEach(card => {
+    card.style.backgroundColor = card.dataset.originalColor;
+  });
   document.body.style.backgroundColor = "white";
 }
