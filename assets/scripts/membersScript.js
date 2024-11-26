c453c99dad1ab297fb6f28c6049943fec63fc825
Theorafael <Theorafaeltitahena@gmail.com>
2024-11-27 01:30:45 +0700

    modified some files. Nov, 27th 2024

--- assets/scripts/membersScript.js
+++ assets/scripts/membersScript.js
@@ -11,11 +11,19 @@ function openNav() {
   allCards.forEach(card => {
     card.style.backgroundColor = "rgba(0,0,0,0.7)";
   });
-  document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
+  let allImg = document.querySelectorAll("img");
+  allImg.forEach(img => {
+    img.style.opacity = "0.1";
+  });
   let allTitles = document.querySelectorAll(".title");
   allTitles.forEach(title => {
     title.style.opacity = "0";
+  });
+  let allContact = document.querySelectorAll("button");
+  allContact.forEach(contact => {
+    contact.style.opacity = "0.1"
   })
+  document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
 }
 
 function closeNav() {
@@ -25,9 +33,17 @@ function closeNav() {
   allCards.forEach(card => {
     card.style.backgroundColor = card.dataset.originalColor;
   });
+  let allImg = document.querySelectorAll("img");
+  allImg.forEach(img => {
+    img.style.opacity = "1";
+  });
   let allTitles = document.querySelectorAll(".title");
   allTitles.forEach(title => {
     title.style.opacity = "1";
+  });
+  let allContact = document.querySelectorAll("button");
+  allContact.forEach(contact => {
+    contact.style.opacity = "1";
   })
   document.body.style.backgroundColor = "white";
 }
